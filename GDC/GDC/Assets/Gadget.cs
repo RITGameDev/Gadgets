ac221565d025698e64f42cc8b95ac23e0f14194b
Jonathan So <jonathan101514@gmail.com>
2015-11-10 19:33:18 -0500

    Update, 10 November 2015. UI Movable Platform

---
+++ GDC/GDC/Assets/Gadget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gadget : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Water") {
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ src/Assets/GM.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GM : MonoBehaviour {
+
+	public float levelTime = 15f;
+
+	public Slider timeMet;
+
+	void Update () {
+	/* Keeps the level time ticking down until the player wins
+	 * the level. */
+		levelTime -= Time.deltaTime;
+		timeMet.value = levelTime;
+		if (levelTime <= 0f){
+			Invoke("NextLvl", 2f); }
+	}
+
+	void NextLvl () {
+	/* Advances the scene to the next level. */
+		Application.LoadLevel("TempWin"); }
+}
